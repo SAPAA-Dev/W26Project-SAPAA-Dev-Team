20d47c00f64bf2fc4946b19b0f4bca7c49c98415
zliang12 <jason.liang5129@gmail.com>
2026-03-07 03:29:41 -0700

    test: finished cypress e2e tests for US-1.0.17

--- webapp/cypress.config.ts
+++ webapp/cypress.config.ts
@@ -1,11 +1,28 @@
 import { defineConfig } from "cypress";
+import { createServerSupabase } from './utils/supabase/server';
+import * as dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 export default defineConfig({
-  allowCypressEnv: true,
   e2e: {
-    baseUrl: 'http://localhost:3000/',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('task', {
+        async cleanupTestQuestion(questionText: string) {
+          const supabase = createServerSupabase();
+          const { data: question } = await supabase
+            .from("W26_questions")
+            .select("id, question_key_id")
+            .eq("form_question", questionText)
+            .single();
+          if (question != null ) {
+            await supabase.from("W26_questions").delete().eq("id", question.id);
+            await supabase.from("W26_question_keys").delete().eq("id", question.question_key_id);
+          }
+          return null;
+        }
+      });
     },
   },
 });
